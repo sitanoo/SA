5a31286a71f2da4690ac6d4ed2618fa408c41955
ismar <ismar@DESKTOP-A3R9DTN>
2019-11-26 22:12:54 -0200

    Correção CRUD operação

--- resources/views/operacao/index.blade.php
+++ resources/views/operacao/index.blade.php
@@ -1,60 +1,74 @@
 @extends('layout')
 @section('conteudo')
+<!-- Conteúdo  -->
+
 
 <script>
-
+                    
     function delOperacao(id, url){
+                        
+        confirma = confirm("Deseja realmente eliminar a operação ?");
+                        
+        if(confirma){
+                        
+            dados = $('#'+id).serialize();
 
-    confirma = confirm("Deseja realmente eliminar a operação ?");
-    if (confirma){
+            $.ajax({
+                method: 'post',
+                url: url,
+                data: dados,
+                dataType: 'html',
+                success: function (data) {
+                    //Mensagem de sucesso
+                    $('#linha'+id).remove();
+                },
+                error: function (argument){
+                    //Mensagem de erro
+                    alert('Operação não eliminada');
+                }
+            });
 
-    dados = $('#' + id).serialize();
-    $.ajax({
-    method: 'post',
-            url: url,
-            data: dados,
-            dataType: 'html',
-            success: function (data) {
-            //Mensagem de sucesso
-            $('#linha' + id).remove();
-            },
-            error: function (argument){
-            //Mensagem de erro
-            alert('Operação não eliminada');
-            }
-    });
-    }
-
+        }
+                    
     return false;
-    }
+                    
+    }                   
 </script>
 
-<div class="card">
-    <div class="col-12" style="padding-left: 1px;">
-        <div class="card">
-            <div class="card-body bg-dark">
-                <h5 class="card-title m-b-0 text-light"><i class="mdi mdi-shopping"></i> Operação</h5>
-            </div>
+<div class="main-content col-12" style="padding-left: 10px;">
+    <div class="card">
+        <div class="card-body bg-dark">
+            <h5 class="card-title m-b-0 text-light"><i class="mdi mdi-shopping"></i> Operações</h5>
         </div>
         <table class="table">
-
-            <tr>
-                <th scope="col">
-                    <a href="/operacao/create" type="button" class="btn btn-outline-success">Adicionar operação</a>
-                </th>
-            </tr>
-
-            <tbody>
-                @foreach ($produto as $p)
-
-                <tr id="linhadel{{$p->ProCod}}">
-                    <th scope="col">{{$p->ProCod}}</th>
-                    <th scope="col">{{$p->ProNom}}</th>
+            <thead>
+                <tr>
+                    <th scope="col">Código Operação</th>
+                    <th scope="col">>Nome Operação</th>
+                    <th scope="col">Código Produto</th>
+                    <th scope="col">Maquina de operação</th>
+                    <th scope="col">*Tempo operação*</th>
                     <th scope="col">
-                        <a href="{{route('produto.edit', $p->ProCod)}}" type="button" class="btn btn-outline-info">Editar</a>
+                        <a href="{{route('operacao.create')}}" type="button" class="btn btn-outline-success">Adicionar Operação</a>
+                    </th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($operacao as $o)
+
+                <tr id="linhadel{{$o->OpCod}}">
+                    <th scope="col">{{$o->OpCod}}</th>
+                    <th scope="col">{{$o->OpNom}}</th>
+                    <th scope="col">{{$o->ProCod}}</th>
+                    <th scope="col">{{$o->OpMaq}}</th>
+                    <th scope="col">{{$o->OpCron}}</th>
+                    <th scope="col">
+                        <a href="{{route('operacao.edit', $o->ProCod)}}" type="button" class="btn btn-outline-info">Editar</a>
                     </th>
                     <th scope="col">
-                        <a href="#" onclick="return delProduto('del{{$p->ProCod}}', '{{route('produto.destroy', $p->ProCod)}}')" type="button" class="btn btn-outline-danger">Excluir</a>
+                        <a href="#" onclick="return delOperacao('del{{$o->ProCod}}', '{{route('operacao.destroy', $o->ProCod)}}')" type="button" class="btn btn-outline-danger">Excluir</a>
                         <form action="" method="post" id="del{{$p->ProCod}}">
                             @csrf
                             @method('DELETE')
@@ -63,29 +77,9 @@
                 </tr>
 
                 @endforeach
-                <tr>
-                    <th scope="col">*Código Operação*</th>
-                    <th >*Nome Operação*</th>
-                    <th scope="col">*Código Produto*</th>
-                    <th scope="col">*Maquina de operação*</th>
-                    <th scope="col">*Tempo operação*</th>
-                    <th scope="col">
-                        <a href="/operacao/2/edit" type="button" class="btn btn-outline-info">Editar</a>
-                    </th>
-                    <th scope="col">
-                        <a href="#" type="button" class="btn btn-outline-danger">Excluir</a>
-                    </th>
-                </tr>
             </tbody>
         </table>
     </div>
-
-</div>   
-
-
-
-
-
-
-
+</div>
 @stop
+<!-- Fim Conteúdo  -->
