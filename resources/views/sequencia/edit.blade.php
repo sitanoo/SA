627588bb9512cce351c76abf7a748bda7d43c70b
brunoamorim616 <47368338+brunoamorim616@users.noreply.github.com>
2019-11-29 20:56:40 -0300

    Ids adicionados

--- resources/views/sequencia/edit.blade.php
+++ resources/views/sequencia/edit.blade.php
@@ -39,62 +39,62 @@
                 <div class="form-group row">
                     <label for="SeqNom" class="col-sm-3 text-left control-label col-form-label">Nome da Sequência</label>
                     <div class="col-sm-9">
-                        <input type="text" value="{{$seq->SeqNom}}" name="SeqNom" required>
+                        <input id="SeqNom" type="text" value="{{$seq->SeqNom}}" name="SeqNom" required>
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="Oppro" class="col-sm-3 text-left control-label col-form-label">Código operação</label>
                     <div class="col-sm-9">
-                         <input type="text" value="{{$seq->OpCod}}" name="OpCod"required>
+                         <input id="OpCod" type="text" value="{{$seq->OpCod}}" name="OpCod"required>
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="ProCod" class="col-sm-3 text-left control-label col-form-label">Código produto</label>
                     <div class="col-sm-9">
-                        <input type="text"  value="{{$seq->ProCod}}" name="ProCod" name="ProCod">
+                        <input id="ProCod" type="text"  value="{{$seq->ProCod}}" name="ProCod" name="ProCod">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqRit" class="col-sm-3 text-left control-label col-form-label">Ritimo</label>
                     <div class="col-sm-9">
-                        <input type="text"  value="{{$seq->SeqRit}}" name="SeqRit" name="SeqRit">
+                        <input id="SeqRit" type="text"  value="{{$seq->SeqRit}}" name="SeqRit" name="SeqRit">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqCon" class="col-sm-3 text-left control-label col-form-label">Sequência Con.</label>
                     <div class="col-sm-9">
-                        <input type="text" value="{{$seq->SeqCon}}" name="SeqCon" name="SeqCon">
+                        <input id="SeqCon" type="text" value="{{$seq->SeqCon}}" name="SeqCon" name="SeqCon">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqQtdVez" class="col-sm-3 text-left control-label col-form-label">Quantidade de vezes</label>
                     <div class="col-sm-9">
-                        <input type="text"  value="{{$seq->SeqQtdVez}}" name="SeqQtdVez" name="SeqQtdVez">
+                        <input id="SeqQtdVez" type="text"  value="{{$seq->SeqQtdVez}}" name="SeqQtdVez" name="SeqQtdVez">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqInt" class="col-sm-3 text-left control-label col-form-label">Intervalo de Sequência</label>
                     <div class="col-sm-9">
-                        <input type="text"  value="{{$seq->SeqInt}}" name="SeqInt" name="SeqInt">
+                        <input id="SeqInt" type="text"  value="{{$seq->SeqInt}}" name="SeqInt" name="SeqInt">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqInt" class="col-sm-3 text-left control-label col-form-label">Sequência por peça</label>
                     <div class="col-sm-9">
-                        <input type="text"  value="{{$seq->SeqPorPec}}" name="SeqPorPec" name="SeqporPec">
+                        <input id="SeqPorPec" type="text"  value="{{$seq->SeqPorPec}}" name="SeqPorPec" name="SeqporPec">
                     </div>
                 </div>
                 
                 <div class="border-top">
                     <div class="card-body">
-                        <button type="submit" class="btn btn-success">Editar</button>
+                        <button id="editar" type="submit" class="btn btn-success">Editar</button>
                     </div>
                 </div>
             </div>
