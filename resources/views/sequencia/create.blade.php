678deaf1fc7cee7d2a4ac3c9b9db967a431cd7ef
brunoamorim616 <47368338+brunoamorim616@users.noreply.github.com>
2019-11-29 21:00:33 -0300

    IDs feitos

--- resources/views/sequencia/create.blade.php
+++ resources/views/sequencia/create.blade.php
@@ -39,56 +39,56 @@
                 <div class="form-group row">
                     <label for="SeqNom" class="col-sm-3 text-left control-label col-form-label">Nome da Sequência</label>
                     <div class="col-sm-9">
-                        <input id="SeqNom" type="text" name="SeqNom" required>
+                        <input id="seqNom" type="text" name="SeqNom" required>
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="Oppro" class="col-sm-3 text-left control-label col-form-label">Código operação</label>
                     <div class="col-sm-9">
-                         <input id="OpCod" type="text" name="OpCod"required>
+                         <input id="opCod" type="text" name="OpCod"required>
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="ProCod" class="col-sm-3 text-left control-label col-form-label">Código produto</label>
                     <div class="col-sm-9">
-                        <input id="ProCod" type="text" name="ProCod" name="ProCod">
+                        <input id="proCod" type="text" name="ProCod" name="ProCod">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqRit" class="col-sm-3 text-left control-label col-form-label">Ritimo</label>
                     <div class="col-sm-9">
-                        <input id="SeqRit" type="text"  name="SeqRit" name="SeqRit">
+                        <input id="seqRit" type="text"  name="SeqRit" name="SeqRit">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqCon" class="col-sm-3 text-left control-label col-form-label">Sequência Con.</label>
                     <div class="col-sm-9">
-                        <input id="SeqCon" type="text" name="SeqCon" name="SeqCon">
+                        <input id="seqCon" type="text" name="SeqCon" name="SeqCon">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqQtdVez" class="col-sm-3 text-left control-label col-form-label">Quantidade de vezes</label>
                     <div class="col-sm-9">
-                    <input id="SeqQtdVez" type="text"  name="SeqQtdVez" name="SeqQtdVez">
+                    <input id="seqQtdVez" type="text"  name="SeqQtdVez" name="SeqQtdVez">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqInt" class="col-sm-3 text-left control-label col-form-label">Intervalo de Sequência</label>
                     <div class="col-sm-9">
-                        <input id="SeqNom" type="text"  name="SeqInt" name="SeqInt">
+                        <input id="seqNom" type="text"  name="SeqInt" name="SeqInt">
                     </div>
                 </div>
                 
                 <div class="form-group row">
                     <label for="SeqInt" class="col-sm-3 text-left control-label col-form-label">Sequência por peça</label>
                     <div class="col-sm-9">
-                        <input id="SeqPorPec" type="text"  name="SeqPorPec">
+                        <input id="seqPorPec" type="text"  name="SeqPorPec">
                     </div>
                 </div>
                 
